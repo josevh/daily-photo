2a24b911c1c4261614e270e7dab88a43f7780138
Jose Herrera <josevictorherrera@gmail.com>
2016-04-28 18:12:53 +0000

    style header and nav

--- components/header/header-image.php
+++ components/header/header-image.php
@@ -15,6 +15,6 @@
 		<?php } else { ?>
 			<div class="site-branding">
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
+				<h3 class="site-description"><?php bloginfo( 'description' ); ?></h3>
 			</div><!-- .site-branding -->
 		<?php } ?>
