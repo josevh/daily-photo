4a9aa772ed41b82f756fecbf3a040902d98992a4
Jose Herrera <josevictorherrera@gmail.com>
2016-04-29 12:40:26 +0000

    update footer content

--- components/footer/site-info.php
+++ components/footer/site-info.php
@@ -1,5 +1,5 @@
 <div class="site-info">
 	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'daily-photo' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'daily-photo' ), 'WordPress' ); ?></a>
 	<span class="sep"> | </span>
-	<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'daily-photo' ), 'Daily Photo', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
+	<?php printf( esc_html__( 'Theme: %1$s by %2$s built on %3$s.', 'daily-photo' ), 'Daily Photo', '<a href="http://josevh.com" rel="designer">Jose Herrera</a>', '<a href="http://components.underscores.me/" rel="designer">Components</a>' ); ?>
 </div><!-- .site-info -->
