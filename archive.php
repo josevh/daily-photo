27b998e054c6f5fd924c09ab4730c0421ee58c3b
Jose Herrera <josevictorherrera@gmail.com>
2016-04-29 14:54:18 +0000

    add styling for archives

--- archive.php
+++ archive.php
@@ -9,7 +9,7 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
+	<div id="primary" class="content-area content-archives">
 		<main id="main" class="site-main" role="main">
 
 		<?php
