26b5fb3e90e4c84dd5a4c2a7e35b4375d715674e
Jose Herrera <josevictorherrera@gmail.com>
2016-04-29 14:05:16 +0000

    format and style comments

--- comments.php
+++ comments.php
@@ -28,14 +28,14 @@ if ( post_password_required() ) {
 		<h2 class="comments-title">
 			<?php
 				printf( // WPCS: XSS OK.
-					esc_html( _nx( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'daily-photo' ) ),
+					esc_html( _nx( 'One comment on &ldquo;%2$s&rdquo;', '%1$s comments on &ldquo;%2$s&rdquo;', get_comments_number(), 'comments title', 'daily-photo' ) ),
 					number_format_i18n( get_comments_number() ),
 					'<span>' . get_the_title() . '</span>'
 				);
 			?>
 		</h2>
 
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : /* Are there comments to navigate through?*/ ?>
 		<nav id="comment-nav-above" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'daily-photo' ); ?></h2>
 			<div class="nav-links">
@@ -45,7 +45,7 @@ if ( post_password_required() ) {
 
 			</div>
 		</nav>
-		<?php endif; // Check for comment navigation. ?>
+		<?php endif; /* Check for comment navigation. */ ?>
 
 		<ol class="comment-list">
 			<?php
@@ -55,7 +55,7 @@ if ( post_password_required() ) {
 				) );
 			?>
 		</ol>
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // Are there comments to navigate through? ?>
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : /* Are there comments to navigate through? */ ?>
 		<nav id="comment-nav-below" class="navigation comment-navigation" role="navigation">
 			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'daily-photo' ); ?></h2>
 			<div class="nav-links">
