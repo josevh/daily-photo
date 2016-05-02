66a067ef5e93bf53e963231450131ca7d541a742
Jose Herrera <josevictorherrera@gmail.com>
2016-05-02 16:01:08 +0000

    handle standard post

--- components/post/content.php
+++ components/post/content.php
@@ -49,5 +49,5 @@
 			) );
 		?>
 	</div>
-	<?php get_template_part( 'components/post/content', 'footer' ); ?>
+	<?php /* get_template_part( 'components/post/content', 'footer' ); */ ?>
 </article><!-- #post-## -->
